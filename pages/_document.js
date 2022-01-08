9ad9cad8e3ca65509037a07b01baf7590532a89c
Harshit <harshitchauhan12102001@gmail.com>
2022-01-08 18:12:03 +0530

    added ai trainer

---
+++ pages/_document.js
@@ -0,0 +1,31 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Layout from '../components/layout'
+import Navbar from '../components/Navbar/Navbar'
+let i=0; 
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    console.log("_document rendered ("+i+")")      
+    return (
+      <Html lang="en">
+        <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="icon/apple-icon-180x180-seochecker-manifest-1908.jpg"></link>
+          <meta name="theme-color" content="#00aae7" />
+        <Head  />
+        <body>
+          <Layout>
+            <Main />
+            <NextScript />
+          </Layout>
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
